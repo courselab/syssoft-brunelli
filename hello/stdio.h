cf4eebdb01a57a89e1cf5254edc948bc472ca0a1
Brunelli <gubrunelli@hotmail.com>
2025-06-14 13:15:12 -0300

    add hello directory

---
+++ hello/stdio.h
@@ -0,0 +1,16 @@
+/*
+ *    SPDX-FileCopyrightText: 2025 Brunelli <gubrunelli@hotmail.com>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
+ *  and includes modifications made by the following author(s):
+ *  Brunelli <gubrunelli@hotmail.com>
+ */
+
+#ifndef E8_H
+#define E8_H
+
+void __attribute__((fastcall, naked)) printf(const char *); 
+
+#endif	
